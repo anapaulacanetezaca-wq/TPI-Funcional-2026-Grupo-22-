871ee0e1999635a9ff2d6b9ad095bbab4e7bdd33
anapaulacanetezaca-wq <anapaulacanetezaca@gmail.com>
2026-06-25 01:21:18 -0300

    Update solucion.ml

--- comparativa/solucion.ml
+++ comparativa/solucion.ml
@@ -1,3 +1,9 @@
+(* ========================================================
+   FUNCIÓN: transicion
+   NATURALEZA: Pura
+   ESTRATEGIA: Pattern matching
+   IMPACTO: No destructiva
+   ======================================================== *)
 let transicion color_actual cambiar_a =
   match (color_actual, cambiar_a) with
   | ("en-rojo", "verde") -> (color_actual, "cambiar-a-verde")
@@ -6,9 +12,18 @@ let transicion color_actual cambiar_a =
   | _ -> (color_actual, "accion-por-defecto")
 ;;
 
+(* ========================================================
+   FUNCIÓN: timer
+   NATURALEZA: Pura
+   ESTRATEGIA: Condicional
+   IMPACTO: No destructiva
+   ======================================================== *)
 let timer timestamp =
-  let posicion_en_ciclo = timestamp mod 216 in
-  if posicion_en_ciclo < 90 then "rojo"
-  else if posicion_en_ciclo < 96 then "amarillo"
-  else "verde"
+  let posicion_en_ciclo = timestamp mod 225 in
+  if posicion_en_ciclo < 90 then "en-rojo"
+  else if posicion_en_ciclo < 93 then "rojo-intermitente"
+  else if posicion_en_ciclo < 213 then "en-verde"
+  else if posicion_en_ciclo < 216 then "verde-intermitente"
+  else if posicion_en_ciclo < 222 then "en-amarillo"
+  else "amarillo-intermitente"
 ;;
